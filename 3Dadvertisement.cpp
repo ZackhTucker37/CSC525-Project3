e9229ac6a4875e9194c30b0fb36a7720a53d34a4
ZackhTucker37 <zrt4026@live.missouristate.edu>
2018-12-11 17:13:15 -0600

    Maybe?

--- 3Dadvertisement.cpp
+++ 3Dadvertisement.cpp
@@ -7,7 +7,7 @@
  CONTRIBUTION BREAKDOWN:	Zackh Tucker -- 50%
 								- basic setup of windows
 								- render the background
-								- render the frame
+								- render the frame 
 								- add camera movement via W-S-A-D keys
 							Josiah McGurty -- 50%
 								- basic setup of windows
@@ -24,170 +24,189 @@
 	3.		Press Ctrl+F5					to EXECUTE
 ==================================================================================================*/
 
-/*
-LodePNG Example
-
-Copyright (c) 2005-2012 Lode Vandevenne
-
-This software is provided 'as-is', without any express or implied
-warranty. In no event will the authors be held liable for any damages
-arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-    1. The origin of this software must not be misrepresented; you must not
-    claim that you wrote the original software. If you use this software
-    in a product, an acknowledgment in the product documentation would be
-    appreciated but is not required.
-
-    2. Altered source versions must be plainly marked as such, and must not be
-    misrepresented as being the original software.
-
-    3. This notice may not be removed or altered from any source
-    distribution.
-*/
-
-//Compile command for Linux:
-//g++ lodepng.cpp 3Dadvertisement.cpp -lSDL -lGL -O3
-
-/*
-LodePNG OpenGL example. Decodes a PNG and shows it in OpenGL. PNG filename
-should be given as a command line parameter.
-
-It's written for the most basic old OpenGL version, and a correction for non
-power of two textures had to be added.
-
-Only very few lines on the sample are about loading the PNG. Most of the
-sample lines show a way to render a texture in 2D in OpenGL.
-
-No fancy 3D graphics are shown, it only shows the image statically. The sample
-shows LodePNG can be used to load PNG images as textures in OpenGL.
-
-/* //
-** //g++ lodepng.cpp 3Dadvertisement.cpp -lSDL -lGL -O3 -o pngTest; ./pngTest ./LightsOut_Resources/galaxy-bg-large.png
-*/ /*
- //
-*/
-
-#include "lodepng.h"
-
 #include <iostream>
-#include <SDL/SDL.h>
-#include <GL/gl.h>
+#include <GL/glut.h>				// include GLUT library
+#include <math.h>
+#include <string>
 
-int main(int argc, char *argv[])
+using namespace std;
+//***********************************************************************************
+
+GLfloat vertices[][3] = { {60, 55, 40}, {60, -50, 40}, {65, 90, -70}, {65, 0, -70}, {-70, 70, 20}, {-70, -20, 20} };
+
+GLfloat colors[][3] = { {1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0} };
+
+int selection;
+
+void drawAxis()
 {
-  if(argc < 2)
-  {
-    std::cout << "Please provide a filename." << std::endl;
-    return 1;
-  }
-  const char* filename = argv[1];
+	char y;
+	y = 'Y';
+	glRasterPos3f(-3, 155, 0);
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, int(y));
 
-  // Load file and decode image.
-  std::vector<unsigned char> image;
-  unsigned width, height;
-  unsigned error = lodepng::decode(image, width, height, filename);
+	char x;
+	x = 'X';
+	glRasterPos3f(155, -3, 0);
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, int(x));
 
-  // If there's an error, display it.
-  if(error != 0)
-  {
-    std::cout << "error " << error << ": " << lodepng_error_text(error) << std::endl;
-    return 1;
-  }
+	char z;
+	z = 'Z';
+	glRasterPos3f(0, 0, 0);
+	glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_10, int(z));
 
-  // Here the PNG is loaded in "image". All the rest of the code is SDL and OpenGL stuff.
+	glPointSize(1);		// change point size back to 1
 
-  int screenw = width;
-  //if(screenw > 1024) screenw = 1024;
-  int screenh = height;
-  //if(screenh > 768) screenw = 768;
+	glBegin(GL_POINTS);	// use points to form X-/Y-axes
+	glColor3f(0, 0, 0);			 // change drawing color to black
+	for (int x = -150; x <= 150; x++) // draw X-axis
+		glVertex3i(x, 0, 0);
+	for (int y = -150; y <= 150; y++) // draw Y-axis
+		glVertex3i(0, y, 0);
+	for (int z = -150; z <= 150; z++) {
+		glVertex3i(0, 0, z);
+	}
+	glEnd();
 
-  if(SDL_Init(SDL_INIT_VIDEO) < 0)
-  {
-    std::cout << "Error: Unable to init SDL: " << SDL_GetError() << std::endl;
-    return 1;
-  }
+}
 
-  SDL_Surface* scr = SDL_SetVideoMode(screenw, screenh, 32, SDL_OPENGL);
+void drawPolygon(int a, int b, int c, int d) {
 
-  if(scr == 0)
-  {
-    std::cout << "Error: Unable to set video. SDL error message: " << SDL_GetError() << std::endl;
-    return 1;
-  }
+	glPolygonMode(GL_BACK, GL_LINE);
+	glPolygonMode(GL_FRONT, GL_FILL);
+	glBegin(GL_POLYGON);
+	glVertex3fv(vertices[a]);
+	glVertex3fv(vertices[b]);
+	glVertex3fv(vertices[c]);
+	glVertex3fv(vertices[d]);
+	glEnd();
+}
 
-  // The official code for "Setting Your Raster Position to a Pixel Location" (i.e. set up a camera for 2D screen)
-  glViewport(0, 0, screenw, screenh);
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0, screenw, screenh, 0, -1, 1);
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
+void drawPipe() {
+	glColor3fv(colors[1]);
+	drawPolygon(0, 1, 3, 2);
 
-  // Make some OpenGL properties better for 2D and enable alpha channel.
-  glDisable(GL_CULL_FACE);
-  glDisable(GL_DEPTH_TEST);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_BLEND);
-  glDisable(GL_ALPHA_TEST);
+	glColor3fv(colors[2]);
+	drawPolygon(5, 4, 2, 3);
 
-  if(glGetError() != GL_NO_ERROR)
-  {
-    std::cout << "Error initing GL" << std::endl;
-    return 1;
-  }
+	glColor3fv(colors[3]);
+	drawPolygon(5, 1, 0, 4);
+}
 
-  // Texture size must be power of two for the primitive OpenGL version this is written for. Find next power of two.
-  size_t u2 = 1; while(u2 < width) u2 *= 2;
-  size_t v2 = 1; while(v2 < height) v2 *= 2;
-  // Ratio for power of two version compared to actual version, to render the non power of two image with proper size.
-  double u3 = (double)width / u2;
-  double v3 = (double)height / v2;
 
-  // Make power of two version of the image.
-  std::vector<unsigned char> image2(u2 * v2 * 4);
-  for(size_t y = 0; y < height; y++)
-  for(size_t x = 0; x < width; x++)
-  for(size_t c = 0; c < 4; c++)
-  {
-    image2[4 * u2 * y + 4 * x + c] = image[4 * width * y + 4 * x + c];
-  }
+//***********************************************************************************
+void myInit()
+{
+	glClearColor(0.8, 0.8, 0.8, 0);			// specify a background color: white 
 
-  // Enable the texture for OpenGL.
-  glEnable(GL_TEXTURE_2D);
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); //GL_NEAREST = no smoothing
-  glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-  glTexImage2D(GL_TEXTURE_2D, 0, 4, u2, v2, 0, GL_RGBA, GL_UNSIGNED_BYTE, &image2[0]);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glEnable(GL_DEPTH_TEST);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glFrustum(-200.0, 200.0, -200.0, 200.0, -200.0, 200.0);
+	glOrtho(-200.0, 200.0, -200.0, 200.0, -200.0, 200.0);
+}
 
-  bool done = false;
-  SDL_Event event = {0};
-  glColor4ub(255, 255, 255, 255);
+//***********************************************************************************
+void myDisplayCallback()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// draw the background
 
-  while(!done)
-  {
-    // Quit the loop when receiving quit event.
-    while(SDL_PollEvent(&event))
-    {
-      if(event.type == SDL_QUIT) done = 1;
-    }
+	drawAxis();
+	drawPipe();
 
-    // Draw the texture on a quad, using u3 and v3 to correct non power of two texture size.
-    glBegin(GL_QUADS);
-      glTexCoord2d( 0,  0); glVertex2f(    0,      0);
-      glTexCoord2d(u3,  0); glVertex2f(width,      0);
-      glTexCoord2d(u3, v3); glVertex2f(width, height);
-      glTexCoord2d( 0, v3); glVertex2f(    0, height);
-    glEnd();
+	glFlush(); // flush out the buffer contents
+}
 
-    // Redraw and clear screen.
-    SDL_GL_SwapBuffers();
-    glClearColor(0, 0, 0, 0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void update() {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	switch (selection) {
+	case 1: glRotatef(45.0, 1.0, 0.0, 0.0); break; //45 degrees around x axis
+	case 2: glRotatef(-45.0, 1.0, 0.0, 0.0); break;
+	case 3: glRotatef(45.0, 0.0, 1.0, 0.0); break;
+	case 4: glRotatef(-45.0, 0.0, 1.0, 0.0); break;
+	case 5: glRotatef(45.0, 0.0, 0.0, 1.0); break;
+	case 6: glRotatef(-45.0, 0.0, 0.0, 1.0); break;
+	}
+	drawPipe();
+	drawAxis();
+	glFlush();
+}
 
-    //Limit frames per second, to not heat up the CPU and GPU too much.
-    SDL_Delay(16);
-  }
+void FirstMenu(int x)
+{
+	if (x == 7) {
+		myDisplayCallback();
+	}
+	if (x == 8) {
+		exit(0);
+	}
+}
+
+void runXMenu(int x) {
+	if (x == 1) {
+		selection = 1;
+	}
+	if (x == 2) {
+		selection = 2;
+	}
+	update();
+}
+
+void runYMenu(int x) {
+	if (x == 1) {
+		selection = 3;
+	}
+	if (x == 2) {
+		selection = 4;
+	}
+	update();
+}
+
+void runZMenu(int x) {
+	if (x == 1) {
+		selection = 5;
+	}
+	if (x == 2) {
+		selection = 6;
+	}
+	update();
+}
+
+
+//***********************************************************************************
+int main(int argc, char ** argv)
+{//glutInit(& argc, argv);                  // optional in our environment
+
+	glutInitWindowSize(400, 400);				// specify a window size
+	glutInitWindowPosition(100, 0);			// specify a window position
+	glutCreateWindow("3D Transformation");	// create a titled window
+	myInit();									// setting up
+
+	glutDisplayFunc(myDisplayCallback);		// register a callback
+
+	int XMenu = glutCreateMenu(runXMenu);
+	glutAddMenuEntry("+45", 1);
+	glutAddMenuEntry("-45", 2);
+
+	int YMenu = glutCreateMenu(runYMenu);
+	glutAddMenuEntry("+45", 1);
+	glutAddMenuEntry("-45", 2);
+
+	int ZMenu = glutCreateMenu(runZMenu);
+	glutAddMenuEntry("+45", 1);
+	glutAddMenuEntry("-45", 2);
+
+	int mainMenu = glutCreateMenu(FirstMenu);
+	glutAddMenuEntry("Help", 1);
+	glutAddSubMenu("Rotate around X axis", XMenu);
+	glutAddSubMenu("Rotate around Y axis", YMenu);
+	glutAddSubMenu("Rotate around Z axis", ZMenu);
+	glutAddMenuEntry("Reset", 7);
+	glutAddMenuEntry("Exit", 8);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
+
+	glutMainLoop();							// get into an infinite loop
+
+	return 0;
 }
